2dec500ca55aa6d6287815485910165101bfb8d2
selcuk.sozuer@sahabt.com <selcuk.sozuer@sahabt.com>
2019-10-15 17:31:22 +0300

    some cleaning

--- src/main/java/com/loadium/jenkins/loadium/model/dto/LoadiumTestBasicDetailsDTO.java
+++ src/main/java/com/loadium/jenkins/loadium/model/dto/LoadiumTestBasicDetailsDTO.java
@@ -1,12 +1,16 @@
 package com.loadium.jenkins.loadium.model.dto;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @Data
 @NoArgsConstructor
+@SuppressFBWarnings(value = { "EI_EXPOSE_REP", "EI_EXPOSE_REP2" }, justification = "I prefer to suppress these FindBugs warnings")
 public class LoadiumTestBasicDetailsDTO {
     private String testKey;
     private String testName;
